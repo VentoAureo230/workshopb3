ea09f157756a417ffd126b354381530a5040ff3b
VentoAureo230 <anthony.meny35@gmail.com>
2023-10-03 09:53:04 +0200

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:workshopb3/pages/home_page.dart';
 import 'package:workshopb3/pages/profile_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_options/firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
