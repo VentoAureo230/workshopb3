4fc4f7dfa7c94b7f428e3fbcb83e6e255ba26ea0
VentoAureo230 <anthony.meny35@gmail.com>
2023-10-03 14:33:14 +0200

    btns

---
+++ lib/components/btn/like_btn.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import '../../theme/colors.dart';
+
+class LikeBtn extends StatelessWidget {
+  final void Function()? onTap;
+  const LikeBtn({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: const Icon(
+        Icons.favorite_rounded,
+        color: AquaColor,
+      ),
+    );
+  }
+}
