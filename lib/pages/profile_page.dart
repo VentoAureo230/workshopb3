ff7e70017d871976db7fac3552a009ea74b73897
VentoAureo230 <anthony.meny35@gmail.com>
2023-10-03 11:59:04 +0200

    structure

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../components/btn_exemple.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -9,18 +8,11 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  
-  void deleteSomething() {} // logique de supression d'un truc
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Profile Page")),
-      body: Column(children: [
-        DeleteButton(onTap: () {
-          deleteSomething(); // t'appelle la logique
-        })
-      ]),
     );
   }
 }
